a6922062515ec2d8515d639e3e2c5f25b23f264c
10974-spec <emmanuelnyakoe13@gmail.com>
2025-09-27 11:55:17 +0300

    just some changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
       <div className="max-w-md w-full space-y-8">
         <div>
           <div className="mx-auto h-12 w-12 bg-primary-500 rounded-full flex items-center justify-center">
-            <span className="text-2xl text-white">🌲</span>
+            <span className="text-2xl text-blue-700">BioDeck</span>
           </div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Sign in to your account
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -44,8 +44,10 @@ const Register = () => {
     <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
+          
           <div className="mx-auto h-12 w-12 bg-primary-500 rounded-full flex items-center justify-center">
-            <span className="text-2xl text-white">🌲</span>
+            
+            <span className="text-2xl text-blue-700">BioDeck</span>
           </div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Create your account
